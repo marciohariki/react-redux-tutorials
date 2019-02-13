5d7998c1c7fb54234f3e3f84917cd41ff4735045
marciohariki <marciohariki@gmail.com>
2019-02-12 22:33:24 -0200

    adding redux

---
+++ streams/client/src/actions/index.js
@@ -0,0 +1,15 @@
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
+
+export const signIn = (userId) => {
+  return {
+    type: SIGN_IN,
+    payload: userId
+  }
+}
+
+export const signOut = () => {
+  return {
+    type: SIGN_OUT
+  }
+}
