f78ba495b911e4835e8cd9e01c05aea222cfcc0e
Márcio Hariki <marciohariki@gmail.com>
2019-02-14 22:08:57 -0200

    adding rest api and listing them with action and reducer

--- streams/client/src/components/streams/StreamCreate.jsx
+++ streams/client/src/components/streams/StreamCreate.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions'
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -24,16 +26,19 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit({ title, description }) {
-    console.log(title)
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+      <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+          className="ui form error"
+      >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput}  label="Enter Description" />
-        <button className="ui button primary">Submit</button>
+        <button className="ui button primary" type="submit">Submit</button>
       </form>
     )
   }
@@ -50,7 +55,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapperd = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapperd)
